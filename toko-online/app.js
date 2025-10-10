05cb0c096ace4a67b5b62d7ab729715b54e79106
Nur Rachmat <rachmat.nur91@gmail.com>
2025-10-10 17:03:02 +0700

    Mongo DB Model

--- toko-online/app.js
+++ toko-online/app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+//panggil file koneksi database
+require("./app_toko_online/models/db");
 
 //Perbaikan ke 2
 var indexRouter = require('./app_toko_online/routes/index');

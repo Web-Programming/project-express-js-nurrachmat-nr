9f0c1d73e01d14f2cd6cc3c15ac9a863889931ba
Nur Rachmat <rachmat.nur91@gmail.com>
2025-10-20 16:59:42 +0700

    Middleware

--- toko-online/app_toko_online/routes/api/order.js
+++ toko-online/app_toko_online/routes/api/order.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const orderController = require('../../controllers/order');
+const auth = require('../middleware/authMiddleware');
 
 // @route   POST /api/orders
 // @desc    Membuat Pesanan Baru
-router.post('/', orderController.create);
+router.post('/', auth.adminOnly, orderController.create);
 
 // @route   GET /api/orders
 // @desc    Mengambil Semua Pesanan (Dibatasi Admin)
@@ -16,6 +17,6 @@ router.get('/:id', orderController.detail);
 
 // @route   PUT /api/orders/:id
 // @desc    Memperbarui Status Pesanan
-router.put('/:id', orderController.update);
+router.put('/:id', auth.adminOnly, orderController.update);
 
 module.exports = router;
